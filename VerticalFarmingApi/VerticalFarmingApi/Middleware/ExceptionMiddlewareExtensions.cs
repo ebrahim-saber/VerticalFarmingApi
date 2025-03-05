9d8896f3c16f05327bb8a26ae29b946c8f0c1523
Ebrahim Saber <121233062+ebrahim-saber@users.noreply.github.com>
2025-03-05 16:12:13 +0200

    Add files via upload

---
+++ VerticalFarmingApi/VerticalFarmingApi/Middleware/ExceptionMiddlewareExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace VerticalFarmingApi.Middleware
+{
+    public static class ExceptionMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseCustomExceptionMiddleware(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<ExceptionMiddleware>();
+        }
+    }
+}
