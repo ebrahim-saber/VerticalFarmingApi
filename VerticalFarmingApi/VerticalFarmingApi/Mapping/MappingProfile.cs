c45ee7a18291b590ddc2555de6482adb4778ae0e
Ebrahim Saber <121233062+ebrahim-saber@users.noreply.github.com>
2025-03-16 21:08:06 +0200

    Add files via upload

---
+++ VerticalFarmingApi/VerticalFarmingApi/Mapping/MappingProfile.cs
@@ -0,0 +1,24 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+using VerticalFarmingApi.Models.DTO;
+using VerticalFarmingApi.Models.DTO__Data_Transfer_Objects_;
+using VerticalFarmingApi.Models;
+using AutoMapper;
+
+namespace VerticalFarmingApi.Mapping
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<User, UserDTO>().ReverseMap();
+            CreateMap<Farm, FarmDto>().ReverseMap();
+            CreateMap<Crop, CropDto>().ReverseMap();
+            CreateMap<Sensor, SensorDto>().ReverseMap();
+            CreateMap<SensorData, SensorDataDto>().ReverseMap();
+            CreateMap<SensorImage, SensorImageDto>().ReverseMap();
+            CreateMap<CropHealthReport, CropHealthReportDto>().ReverseMap();
+            CreateMap<DiseaseAlert, DiseaseAlertDto>().ReverseMap();
+            CreateMap<AIAnalysisResult, AIAnalysisResultDto>().ReverseMap();
+        }
+    }
+}
